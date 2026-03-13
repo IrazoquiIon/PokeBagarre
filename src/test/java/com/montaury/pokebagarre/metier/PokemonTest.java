144e71f9dfa732c14a6f9bd7595d10efeb8f8f71
Irazoqui Ion <iirazoqui001@iutbayonne.univ-pau.fr>
2026-03-13 10:45:23 +0100

    Ajout

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -18,6 +18,7 @@ package com.montaury.pokebagarre.metier;
 import com.montaury.pokebagarre.fixtures.ConstructeurDePokemon;
 import org.junit.jupiter.api.Test;
 
+import static com.montaury.pokebagarre.fixtures.ConstructeurDePokemon.unPokemon;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,8 +26,8 @@ class PokemonTest {
     @Test
     void p1_devrait_gagner_quand_il_a_une_meilleure_attaque() {
         // GIVEN
-        Pokemon p1 = ConstructeurDePokemon.unPokemon().avecAttaque(100).construire();
-        Pokemon p2 = ConstructeurDePokemon.unPokemon().avecAttaque(80).construire();
+        Pokemon p1 = unPokemon().avecAttaque(100).construire();
+        Pokemon p2 = unPokemon().avecAttaque(80).construire();
 
         // WHEN
         boolean resultat = p1.estVainqueurContre(p2);
@@ -38,8 +39,8 @@ class PokemonTest {
     @Test
     void p1_devrait_perdre_quand_le_pokemon_2_a_une_meilleure_attaque() {
         // GIVEN
-        Pokemon p1 = ConstructeurDePokemon.unPokemon().avecAttaque(70).construire();
-        Pokemon p2 = ConstructeurDePokemon.unPokemon().avecAttaque(90).construire();
+        Pokemon p1 = unPokemon().avecAttaque(70).construire();
+        Pokemon p2 = unPokemon().avecAttaque(90).construire();
 
         // WHEN
         boolean resultat = p1.estVainqueurContre(p2);
@@ -51,8 +52,8 @@ class PokemonTest {
     @Test
     void p1_devrait_gagner_quand_les_attaques_sont_egales_mais_p1_a_une_meilleure_defense() {
         // GIVEN
-        Pokemon p1 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(60).construire();
-        Pokemon p2 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(40).construire();
+        Pokemon p1 = unPokemon().avecAttaque(100).avecDefense(60).construire();
+        Pokemon p2 = unPokemon().avecAttaque(100).avecDefense(40).construire();
 
         // WHEN
         boolean resultat = p1.estVainqueurContre(p2);
@@ -64,8 +65,8 @@ class PokemonTest {
     @Test
     void p1_devrait_perdre_quand_les_attaques_sont_egales_mais_p2_a_une_meilleure_defense() {
         // GIVEN
-        Pokemon p1 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(30).construire();
-        Pokemon p2 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(50).construire();
+        Pokemon p1 = unPokemon().avecAttaque(100).avecDefense(30).construire();
+        Pokemon p2 = unPokemon().avecAttaque(100).avecDefense(50).construire();
 
         // WHEN
         boolean resultat = p1.estVainqueurContre(p2);
@@ -76,12 +77,12 @@ class PokemonTest {
 
     @Test
     void p1_devrait_gagner_quand_les_attaques_et_les_defenses_sont_identiques(){
-        //GIVEN
-        Pokemon p1 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(50).construire();
-        Pokemon p2 = ConstructeurDePokemon.unPokemon().avecAttaque(100).avecDefense(50).construire();
-        //WHEN
+        // GIVEN
+        Pokemon p1 = unPokemon().avecAttaque(100).avecDefense(50).construire();
+        Pokemon p2 = unPokemon().avecAttaque(100).avecDefense(50).construire();
+        // WHEN
         boolean resultat = p1.estVainqueurContre(p2);
-        //THEN
+        // THEN
         assertThat(resultat).isTrue();
     }
 
